e20c5d0479678e1a289f13d5aa5b4adc7b1e606f
Andreas Reich (@LAPTOP) <r_andreas2@web.de>
2015-01-23 21:26:06 +0100

    Moved main/game stuff to Game.java

--- Dumbazells/src/Game.java
+++ Dumbazells/src/Game.java
@@ -1,4 +1,49 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class Game {
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+
+public class Game extends BasicGame
+{
+	public Game()
+	{
+		super("Dumbazells");
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {
+		
+	}
+
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException {
+		
+	}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException
+	{
+		g.drawString("Howdy!", 100, 10);
+	}
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			AppGameContainer appgc;
+			appgc = new AppGameContainer(new Game());
+			appgc.setDisplayMode(640, 480, false);
+			appgc.start();
+		}
+		catch (SlickException ex)
+		{
+			Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 
 }
