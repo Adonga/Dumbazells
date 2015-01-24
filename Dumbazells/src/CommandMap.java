a0ecbb565cfe66b4bfbc819b7007ceae0124bf6f
Johannes Jendersie <jojendersie@web.de>
2015-01-24 10:44:13 +0100

    map gradient method

--- Dumbazells/src/CommandMap.java
+++ Dumbazells/src/CommandMap.java
@@ -1,4 +1,4 @@
-import org.lwjgl.util.vector.Vector2f;
+import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -9,6 +9,7 @@ import org.newdawn.slick.geom.Circle;
 public class CommandMap {
 	static final int RESOLUTION_X = 16 * 50;
 	static final int RESOLUTION_Y = 9 * 50;
+	static final int GRADIENT_FILTER_RADIUS = 4;
 	
 	static final private Color[] COMMANDCOLORS = {
 		Color.black, //NOTHING
@@ -65,5 +66,31 @@ public class CommandMap {
 	}
 	private float gameCoordToCommandCoordScale(float scale) {
 		return gameCoordToCommandCoordY(scale);
+	}
+	
+	private boolean onMap(int x, int y) {
+		return x >= 0 && y >= 0 && x < RESOLUTION_X && y < RESOLUTION_Y;
+	}
+	
+	// Get an approximated normal between command and boring areas.
+	// Returns normalized direction vector pointing away from nothing.
+	public Vector2f getBoundaryGradient(Vector2f gamePosition) {
+		// The target vector
+		Vector2f direction = new Vector2f(0,0);
+		// Center coord in map coordinates
+		int cx = (int)(gameCoordToCommandCoordX(gamePosition.x));
+		int cy = (int)(gameCoordToCommandCoordY(gamePosition.y));
+		// Iterate over a circle
+		for(int y = -GRADIENT_FILTER_RADIUS; y <= GRADIENT_FILTER_RADIUS; ++y) {
+			int r = (int)Math.cos(y * 1.570796327f / GRADIENT_FILTER_RADIUS);
+			for(int x = -r; x <= r; ++x) {
+				Vector2f pdir = new Vector2f(x,y);
+				pdir.normalise();
+				if(!onMap(x+cx,y+cy) || commandImageG.getPixel(x+cx, y+cy) == COMMANDCOLORS[CommandType.NOTHING.ordinal()])
+					direction.sub(pdir);
+				else direction.add(pdir);
+			}
+		}
+		return direction.getNormal();
 	}
 }
