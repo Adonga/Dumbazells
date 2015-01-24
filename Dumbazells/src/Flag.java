44a87cc32e1b7dce761753c33c602bac68430ca2
YsenGrimm <kloeten.wars@googlemail.com>
2015-01-24 11:40:39 +0100

    Better Flag spawning

--- Dumbazells/src/Flag.java
+++ Dumbazells/src/Flag.java
@@ -17,13 +17,6 @@ public class Flag {
         this.position = position;
     }
 
-    private boolean positionValid(Vector2f position) {
-
-        // TODO: Implement
-        return true;
-
-    }
-
     public void update(GameContainer gc, int passedTimeMS) {
         if (carriedBy != null) {
             this.position = carriedBy.getPosition();
@@ -45,5 +38,25 @@ public class Flag {
 
     public Bazell getCarriedBy() {
         return carriedBy;
+    }
+
+    /**
+     * Static Stuff
+     */
+
+    public static Vector2f randFlagPosition(Basis[] basen) {
+
+        Vector2f pos = new Vector2f(Flag.FLAG_SIZE + Basis.BASE_SIDE_DEADZONE + (float)Math.random() * (Game.GAME_COORD_SIZE.getX() - 2*(Flag.FLAG_SIZE + Basis.BASE_SIDE_DEADZONE)),
+                Flag.FLAG_SIZE + Basis.BASE_SIDE_DEADZONE + (float)Math.random() * (Game.GAME_COORD_SIZE.getY() - 2*(Flag.FLAG_SIZE + Basis.BASE_SIDE_DEADZONE)));
+
+        for (Basis basis : basen) {
+            Vector2f posCopy = new Vector2f(pos);
+            if (posCopy.sub(basis.getBasePosition()).length() < (Flag.FLAG_BASE_DEADZONE + Basis.BASE_SIZE/2)) {
+                pos = new Vector2f(Flag.FLAG_SIZE + Basis.BASE_SIDE_DEADZONE + (float)Math.random() * (Game.GAME_COORD_SIZE.getX() - 2*(Flag.FLAG_SIZE + Basis.BASE_SIDE_DEADZONE)),
+                        Flag.FLAG_SIZE + Basis.BASE_SIDE_DEADZONE + (float)Math.random() * (Game.GAME_COORD_SIZE.getY() - 2*(Flag.FLAG_SIZE + Basis.BASE_SIDE_DEADZONE)));
+            }
+        }
+
+        return pos;
     }
 }
