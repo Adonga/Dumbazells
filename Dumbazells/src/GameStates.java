458c95c9865cd2333ff552e2d42755d374c11bb6
PStang <paul@stacom.de>
2015-01-24 20:12:08 +0100

    now u dont need a controller but cant play it without one

--- Dumbazells/src/GameStates.java
+++ Dumbazells/src/GameStates.java
@@ -16,29 +16,22 @@ public class GameStates {
 	public boolean gameend;
 	
 	private Controller[] controllers;
+	private boolean[] registerdController;
 	
 	private int registeredPlayers =0;
 	private int i =0;
 	private Image start;
 	private Image bg;
 	private Image win;
+	
 	public GameStates() {
 		
-
-//		try {
-//			start = new Image("images/Start.png");
-//			bg = new Image("images/Background.png");
-//			win = new Image("images/Win.png");
-//		} catch (SlickException e) {
-			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 	}
 
 	public int init(Input input) {
 		
 		int connectedControllers = 0;
-		registeredPlayers = 0;
+//		registeredPlayers = 0;
 
 			// Find controllerIndex'th xbox controller
 			for (int i = 0; i < Controllers.getControllerCount();i++) {
@@ -46,7 +39,8 @@ public class GameStates {
 					++connectedControllers;
 				}
 			}
-			controllers = new Controller[connectedControllers];	
+			controllers = new Controller[connectedControllers];
+			registerdController = new boolean[connectedControllers];
 			int gamePadIndex = 0;
 			for (int i = 0; i < Controllers.getControllerCount();i++) {
 				if (Controllers.getController(i).getName().toLowerCase().contains("xbox")) {
@@ -56,12 +50,16 @@ public class GameStates {
 			}
 			
 			for(int i =0; i<connectedControllers;i++){
-				if(controllers[i].isButtonPressed(0))
+				if(controllers[i].isButtonPressed(0) && registeredPlayers<connectedControllers)
 					registeredPlayers++;
 			}
-			if(controllers[0].isButtonPressed(7) && registeredPlayers>0){
-				registering = false;
-				}
+			if(controllers.length>0)
+			{
+				if(controllers[0].isButtonPressed(7) && registeredPlayers>0){
+					registering = false;
+					}
+			}else
+				if(input.isKeyDown(0)){registering = false;}
 		return registeredPlayers;
 	}
 	
@@ -93,7 +91,7 @@ public class GameStates {
 				g.drawLine( 1 + i, 5 + i, 1 + i, 7 + i);
 			}
 		}
-		if(gameend){
+		else if(gameend){
 				try {
 					if(i!=0){
 					win = new Image("images/Win.png");
